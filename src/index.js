577db1acc0f3af32116f6199efaa11675fbaffdc
Marcel O. Coelho <flasd2@gmail.com>
2018-01-20 04:39:21 -0200

    Wrote module

---
+++ src/index.js
@@ -0,0 +1,24 @@
+function HtmlWebpackDisablePlugin() {}
+
+HtmlWebpackDisablePlugin.prototype.apply = function (compiler) {
+    compiler.plugin('compilation', function (compilation) {
+        compilation.plugin('html-webpack-plugin-alter-chunks', function (chunks) {
+            if (chunks.length > 0) {
+                chunks[0].files = ['false.hot-update.js'];
+            } else {
+                chunks.push({
+                    files: ['false.hot-update.js'],
+                    names: ['haha'],
+                    size: 137,
+                    entry: 'try me',
+                    hash: 'wont emit baby',
+                });
+            }
+            
+
+            return chunks;
+        });
+    });
+};
+
+module.exports = HtmlWebpackDisablePlugin;
